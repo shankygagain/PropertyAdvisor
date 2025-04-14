3f76c5237d8a5ef3dd538609dc6baf01004bb16c
shankygagain <shankypowar26@gmail.com>
2025-04-14 23:27:36 +0530

    jdbc changes

---
+++ src/main/java/com/example/PropertyAdvisor/PropertyAdvisorRepo1.java
@@ -0,0 +1,30 @@
+package com.example.PropertyAdvisor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PropertyAdvisorRepo1 {
+
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<PropertyAdvisor> getAllProp(){
+
+        return jdbcTemplate.query("select * from propertymanager.property", (rs,rownum)-> {
+
+            PropertyAdvisor propertyAdvisor = new PropertyAdvisor();
+            propertyAdvisor.setPropertyId(rs.getInt("id"));
+            propertyAdvisor.setPlaceName(rs.getString("name"));
+            propertyAdvisor.setDimensions(rs.getInt("dimensions"));
+
+
+            return propertyAdvisor;
+
+        });
+    }
+}
